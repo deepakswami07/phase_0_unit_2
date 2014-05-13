0cf1aeb796f74e5c2c843a1bf41c33321040d2d0
daniel <deepakswami07@gmail.com>
2014-05-13 18:24:24 -0500

    pseudocode file

--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -6,20 +6,35 @@
 
 # I worked on this challenge [by myself, with: ].
 
-# 1. Pseudocode
+=begin
+	
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+1. Pseudocode
+
+ What is the input?
+ Array of number or strings
+
+ What is the output? (i.e. What should the code return?)
+ It should return the most frequenlty occuring number or string.
+
+ What are the steps needed to solve the problem?
+ 
+ * define the method
+ * make the method take one argument
+ * to store the frequencies of the numbers or words create an empty hash
+ * go over the array to store keys and values
+ * find the element that is repeated the max times
 
 
-# 2. Initial Solution
+2. Initial Solution
 
 
 
 
-# 3. Refactored Solution
+3. Refactored Solution
 
 
 
-# 4. Reflection 
+4. Reflection 
+
+=end
