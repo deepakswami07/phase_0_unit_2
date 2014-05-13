d1e766a58ac49f9bfc1c11f667fb6266f8b29af3
daniel <deepakswami07@gmail.com>
2014-05-13 15:10:32 -0500

    pseudocode array_total

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -6,23 +6,61 @@
 
 # I worked on this challenge [by myself, with: ].
 
-# 1. Pseudocode
+=begin
+1. Pseudocode
+   a.  Total
+  
+   What is the input?
+   The input in this case is an array
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+   What is the output? (i.e. What should the code return?)
+   Sum of all the numbers in the array
 
+   What are the steps needed to solve the problem?
+   * define a method total
+   * make the method to take one argument
+   * the code within the method shld be
+         * Iterate through the array
+         * add up as it iterates
+         * output the total
+  
+
+   b.  sentence_maker
+
+  What is the input?
+  The input in this case is an array
+
+  What is the output? (i.e. What should the code return?)
+    * Take each word in the array and present it a sentence(string)
+    * The first word of the arrat has to be capitalized.
+    * The sentence should end with a period.
+
+  What are the steps needed to solve the problem?
+  * define the method
+  * make the method take one argument
+  * the code within the method shld be
+     * index the first elment in the array and capitalize it
+     * use the join method to join each element in the array.  Set the output to a variable.
+     * output the variable and add a period at the end of the sentence
+
+=end
 
 # 2. Initial Solution
-
+  
 
 
 
 # 3. Refactored Solution
 
 
+ 
+
+
+
+
+
+
+
+
 
 # 4. Reflection 
-=begin
-	My reflection
-=end
