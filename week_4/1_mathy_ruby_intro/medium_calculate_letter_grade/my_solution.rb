6444e599825b2b31e8b35dceab7311da4808f3ec
daniel <deepakswami07@gmail.com>
2014-05-13 22:03:19 -0500

    file grade

--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -6,12 +6,29 @@
 
 # I worked on this challenge [by myself, with: ].
 
-# 1. Pseudocode
+=begin
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+  1. Pseudocode
 
+  What is the input?
+  array of numbers between 0 and 100
+
+  What is the output? (i.e. What should the code return?)
+  sting of letrer grades
+
+  What are the steps needed to solve the problem?
+
+  Define a method get_grade
+  Set one argument to the method
+  Find the sum of all the numbers
+  Find the average by dividing the sum by total number
+  if average is greater than 90 or equal to, return letter grade A
+  if average is greater than 80 or equal to, return letter grade B
+  if average is greater than 70 or equal to, return letter grade C
+  if average is greater than 60 or equal to, return letter grade D
+  if average is equal to or less than 60, return letter grade F
+
+=end
 
 # 2. Initial Solution
 
@@ -21,5 +38,7 @@
 # 3. Refactored Solution
 
 
+=begin
+ 4. Reflection 
+=end
 
-# 4. Reflection 
