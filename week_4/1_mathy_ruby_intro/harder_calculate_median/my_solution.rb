07fcd1a11911418fd80d859dac438cf87a78c4a5
daniel <deepakswami07@gmail.com>
2014-05-13 22:02:27 -0500

    file median

--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -6,20 +6,43 @@
 
 # I worked on this challenge [by myself, with: ].
 
-# 1. Pseudocode
+=begin 
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+1. Pseudocode
+
+  What is the input?
+  Input would be an array of numbers, both odd and even.
+
+  What is the output? (i.e. What should the code return?)
+  The output should return the middle number in the array.
+
+  What are the steps needed to solve the problem?
+   * define the method
+   * make it take an argument 
+   * sort the array
+   * find out the length of the array
+   * if the length is odd 
+     * divide the length by two, subtract 1 and divided by 2
+     
+   * if the length is even
+   	 * length of the array divided by two and length of the array divided by two subtract one
+
+=end
+ 2. Initial Solution
+   def median(array)
+   sorted = array.sort! 
+   len = sorted.length
+   if len % 2 == 0
+      (sorted[len/2] - 1).to_f + (sorted[len/2]).to_f
+      
+   else
+      sorted[len/2)
+   end
+end
+
+ 3. Refactored Solution
 
 
-# 2. Initial Solution
-
-
-
-
-# 3. Refactored Solution
-
-
-
+=begin
 # 4. Reflection 
+=end
