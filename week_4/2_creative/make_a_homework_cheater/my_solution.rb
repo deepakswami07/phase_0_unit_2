289c27910923c08507762e2deb8d2cc7d0c85ac2
daniel <deepakswami07@gmail.com>
2014-05-14 21:00:01 -0500

    file hw cheat

--- week_4/2_creative/make_a_homework_cheater/my_solution.rb
+++ week_4/2_creative/make_a_homework_cheater/my_solution.rb
@@ -65,7 +65,7 @@ Sub 3:  Cinematograher
 
 
 # 3. Initial Solution
-    def essay_writer(name, nationality, city, pronoun1, name_of_filminudstry, movie_1, pronoun2 
+    def essay_writer(name, nationality, city, pronoun1, name_of_filminudstry, movie_1, pronoun2, 
         movie_2, spouse_name, number_of_childern, number, name_of_book, dod, quote)
 
         "#{name} is a legendary #{nationality} artist, who was born in #{city}.  
@@ -82,7 +82,7 @@ Sub 3:  Cinematograher
 
 
 # 4. Refactored Solution
-      def essay_writer(name, nationality, city, pronoun1, name_of_filminudstry, movie_1, pronoun2 
+      def essay_writer(name, nationality, city, pronoun1, name_of_filminudstry, movie_1, pronoun2, 
         movie_2, spouse_name, number_of_childern, number, name_of_book, dod, quote)
 
         "#{name} is a legendary #{nationality} artist, who was born in #{city}.  
@@ -105,31 +105,51 @@ Sub 3:  Cinematograher
                      "Hollywood",
                      "The Graduate",
                      "his",
-                     "Pappion",
+                     "Papillon",
                      "Diane Keaton",
                      "4",
                      "30",
                      "he",
-                     "My Story'", 
+                     "'My Story'", 
                      "2009", 
-                     "The two basic items necessary to sustain life are sunshine and coconut")
+                     "'The two basic items necessary to sustain life are sunshine and coconut'")
+=begin
+output
+Dustin Hoffman is a legendary American artist, who was born in New York City.Dustin Hoffman is well
+known for Papillon significant contribution to The Graduate. The artistic contribution to the film
+his was what captulated Dustin Hoffman to Papillon road to success.  Dustin Hoffman won an oscar
+for Papillon work in Diane Keaton. Dustin Hoffman was married to 4 and has 30 children. After he
+years in The Graduate, Hollywood wrote a book called 'My Story' in 2009, a few years later
+Hollywood died. Dustin Hoffman is famous for Papillon saying 'The two basic items necessary to
+sustain life are sunshine and coconut'. We will miss Dustin Hoffman immensely.
+
+=end
 
       p essay_writer(
                      "Rekha", 
                      "Indian",
                      "Mumbai",
                      "Bollywood",
-                     "Khoobsoorat",
+                     "Khubsoorat",
                      "her",
                      "Umrao Jaan",
                      "Vinod Mehra",
                      "2",
                      "25",
                      "she",
-                     "Kahaani", 
+                     "'Kahaani'", 
                      "2012", 
-                     "Being alone is not necessarily being lonely!")
+                     "'Being alone is not necessarily being lonely!'")
 
+=begin
+output
+Rekha is a legendary Indian artist, who was born in Mumbai.Rekha is well known for Umrao Jaan significant
+contribution to Khubsoorat. The artistic contribution to the film her was what captulated Rekha to Umrao
+Jaan road to success.  Rekha won an oscar for Umrao Jaan work in Vinod Mehra. Rekha was married to 2 and
+has 25 children. After she years in Khubsoorat, Bollywood wrote a book called 'Kahaani' in 2012, a few
+years later Bollywood died. Rekha is famous for Umrao Jaan saying 'Being alone is not necessarily being
+lonely!'. We will miss Rekha immensely.
+=end
       p essay_writer(
                      "Genevieve Nnaji", 
                      "Nigerian",
@@ -142,10 +162,21 @@ Sub 3:  Cinematograher
                      "2",
                      "12",
                      "she",
-                     "My Life", 
+                     "'My Life'", 
                      "2012", 
-                     "Chocolates and cakes. My biggest problem!")
+                     "'Chocolates and cakes. My biggest problem!'")
 
+
+=begin
+output
+Genevieve Nnaji is a legendary Nigerian artist, who was born in Lagos.Genevieve Nnaji is well known for
+30 Days significant contribution to Camouflage. The artistic contribution to the film her was what
+captulated Genevieve Nnaji to 30 Days road to success.  Genevieve Nnaji won an oscar for 30 Days
+work in Ramsey. Genevieve Nnaji was married to 2 and has 12 children. After she years in Camouflage,
+Nollywood wrote a book called 'My Life' in 2012, a few years later Nollywood died. Genevieve Nnaji
+is famous for 30 Days saying 'Chocolates and cakes. My biggest problem!'. We will miss Genevieve
+Nnaji immensely.
+=end
 
 
 # 5. Reflection 
@@ -156,7 +187,7 @@ Sub 3:  Cinematograher
  the output needed to be and how best it can be used for different person/subject etc.
  Once I got that part, the rest was making sure I got the pronouns in the right place.
  I found thi new learnign experience a good place to start using driver codes. It was not very
- tedious, but I had to make sure there were no mistakes.  Overall, I did enjoy it, as always
- it take me longer to understand and get things done.
+ tedious, but I had to make sure there were no mistakes.  Also, it did not require much refacotring.
+ Overall, I did enjoy it, as always it take me longer to understand and get things done.
 
 =end
