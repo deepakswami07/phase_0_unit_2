5afc1364c27caacbfa76b74f1cb5ef77bad21b0c
daniel <deepakswami07@gmail.com>
2014-05-21 12:22:05 -0500

    files 123

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,45 +1,71 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
+
+# Input: Create a new object. Enter the number of sides of a dice(argument)
+
+# Output: Gives the number of sides and also pick a random number out of the number of sides given.
+
 
 # 2. Pseudocode
-
-# Input:
-# Output:
-# Steps:
+=begin
+    
+     * Set attribute accessor for sides
+     * Set instance variable for sides
+     * Raise argument error if argument is less than 1
+     * Set the rand argument to sides + 1 to the roll method
+=end
 
 
 # 3. Initial Solution
 
-class Die
-  def initialize(sides)
-    # code goes here
-  end
-  
-  def sides
-    # code goes here
-  end
-  
-  def roll
-    # code goes here
-  end
-end
+	class Die
+		attr_accessor :sides
 
+	  def initialize(sides)
+	    @sides = sides
+	    raise ArgumentError.new("Enter a number greater than 0")  unless sides > 1
+	  end
+	  
+	  def roll
+	    return rand(sides) + 1
+	  end
+	end
 
 
 # 4. Refactored Solution
 
+	class Die
+		attr_accessor :sides
 
-
-
+	  def initialize(sides)
+	    @sides = sides
+	    raise ArgumentError.new("Enter a number greater than 0")  unless sides > 1
+	  end
+	  
+	  def roll
+	  rand(sides) + 1
+	  end
+	end
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+  # test new object of class
+  object_test = Die.new(6)
 
+  # test correct number of sides
+  p object_test.sides == 6
 
-
-
+  # test random call
+  p (1..6).include?(object_test.roll)
 
 
 # 5. Reflection 
+=begin
+      I found this challenge to be fairly easy.  Reading the resources provided before working
+      on this challenge made me more comfortable with class. How newly created objects are related
+      to each class, and how methods defined in the class can be applied to new objects gave me a
+      better understanding of objects and methods.  I hope to build on this knowledge as I move on
+      to more challenging exercises.
+=end
--- week_5/3_guessing_game_solo_challenge/my_solution.rb
+++ week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -1,40 +1,112 @@
 # U2.W5: Build a simple guessing game SOLO CHALLENGE
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge myself.
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: Create a new object of class GuessingGame with an argument.  
+# Output: 
+  # when you call call the method guess with an argument, 
+  # you will get three answers, high(guess > answer) low(guess < answer)
+  # If you called the method solved?, you will get a True or False depending on the answer
+
 # Steps:
+# SET  instance variable for answer
+# CREATE an instance variable that checks to see if the guess is true/false, but starts as false.
+# DEFINE method guess with one argument(integer)
+# IF condition, if guess is less than answer THEN low
+# ElSIF condition, if guess is greater than answer THEN high
+# ELSE condtion guess equals answer THEN correct
+# DEFINE method solved? 
+# IF condition to boolean T/F
 
 
 # 3. Initial Solution
 
-class GuessingGame
-  def initialize(answer)
-    # Your initialization code goes here
-  end
-  
-  # Make sure you define the other required methods, too
+	class GuessingGame
+
+	    def initialize(integer)
+		  @answer = integer
+		  @last_guess = false
+		  raise ArgumentError.new("Enter an integer to continue") unless @answer != Integer
+	    end
+
+		def guess(guess_number)
+			@guess_number = guess_number
+
+	  	if  @answer < guess_number
+	  		@last_guess
+	  		return :high
+	  		
+
+	  	elsif @answer == guess_number
+	  		@last_guess = true
+	  		return :correct
+	  		
+
+	  	else @answer > guess_number
+	  		@last_guess
+	  		return :low
+	  	end
+	  end
+
+	    def solved?
+	    	@last_guess
+	   end
 end
-
-
 
 
 # 4. Refactored Solution
 
+class GuessingGame
 
+	    def initialize(integer)
+		  @answer = integer
+		  @last_guess = false
+	    end
 
+		def guess(guess_number)
+	  	if  @answer < guess_number
+	  		return :high
+	  	elsif @answer == guess_number
+	  		@last_guess = true
+	  		return :correct
+	  	else @answer > guess_number
+	  		return :low
+	  	end
+	  end
 
+	    def solved?
+	    	@last_guess
+	   end
+end
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+   #test object
+  my_game = GuessingGame.new(12)
+  
+  #test solved? before
+  my_game.solved? == false
 
+  #test low
+  p my_game.guess(10) == :low
 
+  #test high
+  p my_game.guess(18) == :high
 
+  #test solved? after
+  my_game.solved? == true
 
-
-
+  
 # 5. Reflection 
+=begin
+	  This was good introduction to class.  I was wondering how even
+	  without setting the guess_number variable it still worked. I need
+	  to do more class problems to get a deeper understanding of how class
+	  works and how it is apllied.  Even though I did not use attr
+	  (reader, writer and accessor), I learned how they can be used.  
+=end
+	
+
